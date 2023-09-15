6cce909fa741df7c11cb4883733da36191816f4d
Yaseen Ahmad <yaseenahmad402@gmail.com>
2023-09-16 02:31:17 +0500

    Fixed the error

--- components/ui/ColorPicker.tsx
+++ components/ui/ColorPicker.tsx
@@ -12,21 +12,23 @@ export default function ColorPicker({ type }: ColorPickerProps) {
   const defaultPrimary = "#690FAD";
   const defaultSecondary = "#CAA8F5";
 
-  const defaultColor = localStorage.getItem(type) || (type === 'primary' ? defaultPrimary : defaultSecondary);
-  const [color, setColor] = useState(defaultColor);
+  const [color, setColor] = useState<string>("");
 
   useEffect(() => {
+    const storedColor = localStorage.getItem(type) || (type === 'primary' ? defaultPrimary : defaultSecondary);
+    setColor(storedColor);
+
     localStorage.setItem(type, color);
     updateColors(localStorage.getItem('primary') || defaultPrimary, localStorage.getItem('secondary') || defaultSecondary);
   }, [type, color]);
 
   return (
-    <input 
-      type="color" 
-      className={colorInput} 
-      value={color} 
-      onChange={e => setColor(e.target.value)} 
+    <input
+      type="color"
+      className={colorInput}
+      aria-label='color'
+      value={color}
+      onChange={e => setColor(e.target.value)}
     />
   );
 }
-
